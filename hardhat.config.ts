400d4dd195b0ac90aebd031d4d1d3ca8e5740a54
Renning Bruns <nullren@github.com>
2021-12-07 14:48:05 -0800

    always report gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,7 +32,8 @@ const config: HardhatUserConfig = {
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    // enabled: process.env.REPORT_GAS !== undefined,
+    enabled: true,
     currency: "USD",
   },
   etherscan: {
